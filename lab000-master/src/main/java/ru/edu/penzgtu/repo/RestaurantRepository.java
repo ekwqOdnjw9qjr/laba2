a60944e9f062d1b8605db5d6088ea832fbbdbce6
zxc <psychokid534@gmail.com>
2024-05-22 21:19:38 +0300

    1

--- lab000-master/src/main/java/ru/edu/penzgtu/repo/RestaurantRepository.java
+++ lab000-master/src/main/java/ru/edu/penzgtu/repo/RestaurantRepository.java
@@ -1,6 +1,8 @@
 package ru.edu.penzgtu.repo;
 
+import org.springframework.stereotype.Repository;
 import ru.edu.penzgtu.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
 }
